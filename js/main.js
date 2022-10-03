fb7a3ab927032247d7289a2ea0013cb6a451505c
SHIKAMUSENGE Philemon <shikamusenge720@gmail.com>
2022-10-03 15:42:53 +0200

    email chake l 5

--- js/main.js
+++ js/main.js
@@ -2,5 +2,6 @@ let btn = document.querySelector("#sms-btn");
 btn.addEventListener("click", send);
 function send(e) {
   e.preventDefault;
+  if(document.querySelector("input").value)
   alert("Message is sent success \n full thank you");
 }

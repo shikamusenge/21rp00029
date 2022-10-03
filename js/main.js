2bfaed7535467a06083fac035524c562deef9b67
shikamusenge <111120523+shikamusenge@users.noreply.github.com>
2022-10-03 08:25:04 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let btn = document.querySelector("#sms-btn");
+btn.addEventListener("click", send);
+function send(e) {
+  e.preventDefault;
+  alert("Message is sent success \n full thank you");
+}
